0ed16e4e163078f6a3596648ed61d36397c1b8f1
arthur-kam <arthur.kam@mail.utoronto.ca>
2022-02-07 17:49:14 -0800

    stuff

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var log = console.log;
 
 var tag = document.createElement('script');
-tag.src = "http://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
@@ -12,24 +12,24 @@ var times = {
 };
 
 // default video: YOASOBI - Racing Into The Night (Yoru ni Kakeru) / THE HOME TAKE
-YORI_NI_KAKERU = 'j1hft9Wjq9U'
+DEFAULT_VIDEO_ID = 'j1hft9Wjq9U';
 
-var videoId = YORI_NI_KAKERU; 
+var videoId = DEFAULT_VIDEO_ID; 
 var vidLength;
 var startInput, endInput, urlInput;
 var timeoutId;
 
+FORMATTED_START_TIME_ID = 'formattedStartTime';
+FORMATTED_END_TIME_ID = 'formattedEndTime';
+
 var player;
 window.onYouTubeIframeAPIReady = function() {
     player = new YT.Player('player', {
-        // height: '360',
-        // width: '640',
         height: '480',
         width: '854',
         videoId: videoId,
         playerVars: {
             modestbranding: 1,
-            rel: 0,
             autoplay: 1,
             enablejsapi: 1,
             html5: 1,
@@ -45,30 +45,45 @@ window.onYouTubeIframeAPIReady = function() {
 }
 
 function onReady(event) {
-    log('Player ready');
+    // log('Player ready');
     vidLength = Math.floor(player.playerInfo.duration);
-    // upon loading new video, set start time to 0 and end time to match end of video 
-    times.start = 0;
-    times.end = vidLength;
+    setStartAndEndTimes();
+    syncTimeFields();
+    setInputValueCeilings();
 
-    endInput.value = times.end;
-
-    endInput.setAttribute('max', times.end.toString());
-    startInput.setAttribute('max', times.end.toString());
-   
     player.seekTo(times.start);
     player.playVideo();
 }
 
+function setStartAndEndTimes() { 
+    times.start = 0;
+    times.end = vidLength;
+}
+
+function syncTimeFields() {
+    startInput.value = times.start;
+    endInput.value = times.end;
+    resetFormattedTimeDisplays();
+}
+
+function resetFormattedTimeDisplays() {
+    showFormattedTime(startInput, FORMATTED_START_TIME_ID)();
+    showFormattedTime(endInput, FORMATTED_END_TIME_ID)();
+}
+
+function setInputValueCeilings() {
+    startInput.setAttribute('max', times.end.toString());
+    endInput.setAttribute('max', times.end.toString());
+}
+
 function onStateChange(event) {
-    log('Player state change')
+    // log('Player state change')
     if (event.data == YT.PlayerState.PLAYING) {
         var end = Math.min(times.end, vidLength);
         var start = Math.max(player.getCurrentTime(), times.start, 0);
         var duration = end - start;
         clearTimeout(timeoutId);
         timeoutId = setTimeout(restartVideoSection, duration * 1000);
-
     } 
 }
 
@@ -82,7 +97,7 @@ function restartVideoSection() {
 
 function reloadPlayer() {
     if (player) {
-        log('Destroyed player');
+        // log('Destroyed player');
         player.destroy();
     }
     onYouTubeIframeAPIReady();
@@ -97,7 +112,7 @@ function updateStart() {
     if (player.playerInfo.currentTime < times.start) {
         restartVideoSection();
     }
-    console.log('Start time updated to', times.start);
+    log('Start time updated to', times.start);
     if (times.start > times.end) {
         times.end = times.start;
         endInput.value = times.start;
@@ -109,7 +124,7 @@ function updateEnd() {
     if (times.end < player.playerInfo.currentTime) {
         restartVideoSection();
     }
-    console.log('End time updated to', times.end);
+    log('End time updated to', times.end);
     if (times.end < times.start) {
         times.start = times.end;
         startInput.value = times.end;
@@ -118,69 +133,67 @@ function updateEnd() {
 
 function updateUrl() {
     var url = urlInput.value;
-    log('Current video ID:', videoId);
+    log('Current video - ID:', videoId);
     videoId = getVideoId(url);
     log('New video requested - ID:', videoId);
-    
     reloadPlayer();
 }
 
 function getVideoId(url) {
-    if (!(url)) {
-        log('no URL found, defaulting to', YORI_NI_KAKERU);
-        return YORI_NI_KAKERU;
+    if (!url) {
+        log('no URL found, defaulting to', DEFAULT_VIDEO_ID);
+        return DEFAULT_VIDEO_ID;
     }
-    var ID = '';
+    var newId = '';
     url = url.replace(/(>|<)/gi,'').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
     if(url[2] !== undefined) {
-        ID = url[2].split(/[^0-9a-z_\-]/i);
-        ID = ID[0];
+        newId = url[2].split(/[^0-9a-z_\-]/i);
+        newId = newId[0];
     } else {
         try {
-            ID = url[0];
+            newId = url[0];
         } catch (e) {
-            ID = url;
+            newId = url;
         }
     }
-    log(typeof ID)
-    return ID;
+    return newId;
 }
 
-function prettyPrintSeconds(s) {
-    // given a number representing seconds, formats into a string of mm:ss if the duration represneted is less than 1 hour, and hh:mm:ss otherwise.
-    // 1 hour = 3600 seconds
-    if (s < 3600) {
-        return `${Math.trunc(s / 60)}:${s % 60}`;
+function formatSeconds(s) {
+    // given a number representing seconds, formats into a string of mm:ss if the duration represented is less than 1 hour, and hh:mm:ss otherwise.
+    var secondsInAnHour = 60 * 60;
+    var allMins = Math.trunc(s / 60);
+    var seconds = s % 60;
+    var minutes = allMins % 60;
+    var padSeconds = seconds > 9 ? '' : '0';
+    var padMinutes = minutes > 9 ? '' : '0';
+    if (s < secondsInAnHour) {
+        return `${minutes}:${padSeconds}${seconds}`;
     } else {
-        allMins = Math.trunc(s / 60);
-        seconds = s % 60;
-        hours = Math.trunc(allMins / 60);
-        minutes = allMins % 60;
-        return `${hours}:${minutes}:${seconds}`
+        var hours = Math.trunc(allMins / 60);
+        var padHours = hours > 9 ? '' : '0';
+        return `${padHours}${hours}:${padMinutes}${minutes}:${padSeconds}${seconds}`;
     }
 }
 
-function validateEnd(event) {
-    if (endInput.validity.rangeOverflow) {
-        endInput.value = vidLength;
-    }
-    if (endInput.validity.rangeUnderflow || endInput.validity.valueMissing) {
-        endInput.value = 0;
-    }
-    if (endInput.validity.stepMismatch) {
-        endInput.value = parseInt(endInput.value);
+function validateTime(inputElement) {
+    return function() {
+        if (inputElement.validity.rangeOverflow) {
+            inputElement.value = vidLength;
+        }
+        if (inputElement.validity.rangeUnderflow || inputElement.validity.valueMissing) {
+            inputElement.value = 0;
+        }
+        if (inputElement.validity.stepMismatch) {
+            inputElement.value = parseInt(inputElement.value);
+        }
     }
 }
 
-function validateStart(event) {
-    if (startInput.validity.rangeOverflow) {
-        startInput.value = vidLength;
-    }
-    if (startInput.validity.rangeUnderflow || startInput.validity.valueMissing) {
-        startInput.value = 0;
-    }
-    if (startInput.validity.stepMismatch) {
-        startInput.value = parseInt(startInput.value);
+function showFormattedTime(element, formattedDisplayId) {
+    return function() {
+        var formattedStr = formatSeconds(element.value);
+        document.getElementById(formattedDisplayId).innerHTML = formattedStr;
     }
 }
 
@@ -188,12 +201,16 @@ function validateStart(event) {
 window.onload = (function() {
     startInput = document.querySelector('#startTimeInput');
     startInput.addEventListener('change', updateStart);
-    startInput.addEventListener('input', validateStart);
+    startInput.addEventListener('input', validateTime(startInput));
+    startInput.addEventListener('input', showFormattedTime(startInput, FORMATTED_START_TIME_ID))
 
     endInput = document.querySelector('#endTimeInput');
     endInput.addEventListener('change', updateEnd);
-    endInput.addEventListener('input', validateEnd);
+    endInput.addEventListener('input', validateTime(endInput));
+    endInput.addEventListener('input', showFormattedTime(endInput, FORMATTED_END_TIME_ID))
 
     urlInput = document.querySelector('#urlInput');
     urlInput.addEventListener('change', updateUrl);
+    urlInput.addEventListener('change', updateStart);
+    urlInput.addEventListener('change', updateEnd);
 })();
